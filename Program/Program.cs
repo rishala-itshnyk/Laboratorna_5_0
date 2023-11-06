dd0875b96787a1a737cdbd859f5763c2cb109ab0
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2023-11-06 02:20:05 +0200

    initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main()
+        {
+            int a, b;
+
+            Console.Write("a = ? ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("b = ? ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int c = Sum(a, b);
+            Console.WriteLine("c = " + c);
+
+            Console.ReadLine();
+        }
+    }
+}
